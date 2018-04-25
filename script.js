2e0745676b1fbf8f9a9bfd7ccaf53b5af7d68f29
Dawid <dawidw1999@gmail.com>
2018-04-25 11:31:27 +0200

    1.0
    Final version

--- script.js
+++ script.js
@@ -21,32 +21,21 @@ $(document).ready(function () {
         tile1 = "",
         tile2 = "",
         shot = 1,
-        tilesClass = 0;
+        tilesClass = 0,
+        tilesFound = 0,
+        sec = 0,
+        time = setInterval(function(){stoper()}, 1000);
 
-    //Generowanie kafelków w losowej kolejności (nie dziala wtedy odkrywanie)
-
-    /* $(".startButton").click(function () {
-         //$(".board").empty();
-         for (var y = 0; y < 2; y++) {
-             for (var x = 0; x < 15; x++) {
-                 var randomNumber = Math.floor(Math.random() * 100);
-
-                 $(".board").prepend("<img class='tilesClosed' src='img/" + tilesImg[x] + "' style='order:" + randomNumber + "' >");
-             }
-         }
-     });*/
+    function stoper(){
+        sec++;
+    };
+    
+    //Generowanie kafelków w losowej kolejności + restart gry
 
     $(".startButton").click(function () {
-        shot = 1;
+        time;
         $('.tiles').empty();
-        /* for (var z = 0; z < 2; z++) {
-             for (var y = 0; y < 15; y++) {
-                 var randomNumber = Math.floor(Math.random() * 100);
-
-
-                 $(".board").prepend("<div class='tilesClosed " + "img" + y + "' ' style='order:" + randomNumber + "' ></div>");
-             }
-         }*/
+        shot = 1;
         for (var x = 0; x < 30; x++) {
             tilesClass++;
             if (tilesClass == 15) {
@@ -55,8 +44,6 @@ $(document).ready(function () {
             var randomNumber = Math.floor(Math.random() * 100);
 
             $('.tiles').eq(x).css('order', randomNumber);
-            //$('.tiles').eq(x).addClass('img' + tilesClass);
-            //$('.tiles').eq(x).addClass('tilesClosed');
             $('.tiles').eq(x).append("<img class='tilesOpened' src='img/" + tilesImg[tilesClass] + "'>");
             $('.tiles').eq(x).prepend("<img class='tilesClosed' src='img/logo.png'>");
 
@@ -68,24 +55,27 @@ $(document).ready(function () {
     //Odkrywanie i sprawdzanie kafelków 
 
     $(".tiles").click(function () {
+        //Blokowanie mozliwości kliknięcia w ten sam kafelek
         if ($(this).hasClass('chosen') == false) {
+            
+            //Odwrócenie pierwszego kafelka
             if (countOfTiles === 0) {
-                //$(this).addClass("tilesOpened");
                 $(this).children('.tilesOpened').toggle('display');
                 $(this).children('.tilesClosed').toggle('display');
                 $(this).addClass('chosen');
                 countOfTiles++;
                 tile1 = $(this).children('.tilesOpened').attr('src');
+                
+                //Odwrócenie drugiego kafelka
             } else if (countOfTiles === 1) {
-                console.log(shot);
-
                 shot++;
-                //$(this).addClass("tilesOpened");
                 $(this).children('.tilesOpened').toggle('display');
                 $(this).children('.tilesClosed').toggle('display');
                 $(this).addClass('chosen');
                 countOfTiles++;
                 tile2 = $(this).children('.tilesOpened').attr('src');
+
+                //Jeśli nie pasuja
                 if (tile1 !== tile2) {
                     setTimeout(function () {
                         $('.chosen').children('.tilesOpened').css('display', 'none');
@@ -94,15 +84,31 @@ $(document).ready(function () {
                         countOfTiles = 0;
 
                     }, 1000);
+
+                    //Jeśli pasują
                 } else {
+                    tilesFound++;
+                    $('.chosen').animate({
+                        opacity: 0
+                    }, 1100);
                     setTimeout(function () {
+
                         $('.chosen').addClass('found');
                         $('.chosen').removeClass('chosen');
                         countOfTiles = 0;
 
-                    }, 700);
-                }
-            }
+                        //Koniec gry
+                        if (tilesFound == 15) {
+                            clearInterval(time);
+                            $('.board').css({
+                                display: 'none'
+                            });
+                            $('.bravo').css('display', 'flex');
+                            $('.bravo').prepend("Gratulacje!<br>Udało Ci się odgadnąć wszystkie kafelki po " + shot + " próbach, w czasie "+sec+" sekund. Świetny wynik :-)");
+                        };
+                    }, 1100);
+                };
+            };
         };
     });
 });
